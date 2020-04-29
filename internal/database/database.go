654ef7e4c45b7cd18dac12177cc530250a478a53
jose <josetxu1996@gmail.com>
2020-04-29 11:42:49 +0200

    change model

--- internal/database/database.go
+++ internal/database/database.go
@@ -27,21 +27,21 @@ var DB Store
 // CreateRecipe : adds a recipe to the database
 func (store *DbStore) CreateRecipe(recipe *model.Recipe) error {
 	s, _ := json.Marshal(recipe.Ingredients)
-	_, err := store.Db.Query("INSERT INTO recipes(name, description, ingredients, people) VALUES ($1,$2,$3,$4)", recipe.Name, recipe.Description, s, recipe.People)
+	_, err := store.Db.Query("INSERT INTO recipes(name, description, ingredients, people, instructions) VALUES ($1,$2,$3,$4,$5)", recipe.Name, recipe.Description, s, recipe.People, recipe.Instructions)
 	return err
 }
 
 // UpdateRecipe : updates a recipe from the database
 func (store *DbStore) UpdateRecipe(recipe *model.Recipe, name string) error {
 	s, _ := json.Marshal(recipe.Ingredients)
-	_, err := store.Db.Exec("UPDATE recipes SET name=$1, description=$2, ingredients=$3, people=$4 WHERE name=$5", recipe.Name, recipe.Description, s, recipe.People, name)
+	_, err := store.Db.Exec("UPDATE recipes SET name=$1, description=$2, ingredients=$3, people=$4, instructions=$5 WHERE name=$6", recipe.Name, recipe.Description, s, recipe.People, recipe.Instructions, name)
 	return err
 }
 
 // Getrecipes : gets all recipes from the database
 func (store *DbStore) GetRecipes() ([]*model.Recipe, error) {
 
-	rows, err := store.Db.Query("SELECT name, description, ingredients, people FROM recipes")
+	rows, err := store.Db.Query("SELECT name, description, ingredients, people, instructions FROM recipes")
 
 	if err != nil {
 		return nil, err
@@ -53,7 +53,7 @@ func (store *DbStore) GetRecipes() ([]*model.Recipe, error) {
 
 		recipe := &model.Recipe{}
 		var s []byte
-		if err := rows.Scan(&recipe.Name, &recipe.Description, &s, &recipe.People); err != nil {
+		if err := rows.Scan(&recipe.Name, &recipe.Description, &s, &recipe.People, &recipe.Instructions); err != nil {
 			return nil, err
 		}
 		json.Unmarshal(s, &recipe.Ingredients)
@@ -65,11 +65,11 @@ func (store *DbStore) GetRecipes() ([]*model.Recipe, error) {
 // GetRecipe : gets a recipe from the database
 func (store *DbStore) GetRecipe(name string) (model.Recipe, error) {
 
-	row := store.Db.QueryRow("SELECT name, description, ingredients, people FROM recipes where name = $1", name)
+	row := store.Db.QueryRow("SELECT name, description, ingredients, people, instructions FROM recipes where name = $1", name)
 
 	recipe := model.Recipe{}
 	var s []byte
-	err := row.Scan(&recipe.Name, &recipe.Description, &s, &recipe.People)
+	err := row.Scan(&recipe.Name, &recipe.Description, &s, &recipe.People, &recipe.Instructions)
 	if err != nil {
 		return model.Recipe{}, err
 	}
--- internal/model/recipe/recipe.go
+++ internal/model/recipe/recipe.go
@@ -2,8 +2,9 @@ package Recipe
 
 // Recipe : recipes model
 type Recipe struct {
-	Name        string         `json:"name" validate:"required"`
-	Description string         `json:"description" validate:"required"`
-	People      int            `json:"people" validate:"required"`
-	Ingredients map[string]int `json:"ingredients" validate:"required"`
+	Name         string         `json:"name" validate:"required"`
+	Description  string         `json:"description" validate:"required"`
+	People       int            `json:"people" validate:"required,numeric"`
+	Ingredients  map[string]int `json:"ingredients" validate:"required"`
+	Instructions string         `json:"instructions" validate:"required"`
 }
