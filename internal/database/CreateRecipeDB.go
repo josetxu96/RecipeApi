1e6c26df8baedbda9884f61d0a5f9fa72f731136
jose <josetxu1996@gmail.com>
2020-05-01 10:35:17 +0200

    error handleling fix

--- internal/database/CreateRecipeDB.go
+++ internal/database/CreateRecipeDB.go
@@ -7,7 +7,10 @@ import (
 
 // CreateRecipe : adds a recipe to the database
 func (store *DbStore) CreateRecipe(recipe *model.Recipe) error {
-	s, _ := json.Marshal(recipe.Ingredients)
-	_, err := store.Db.Query("INSERT INTO recipes(name, description, ingredients, people, instructions) VALUES ($1,$2,$3,$4,$5)", recipe.Name, recipe.Description, s, recipe.People, recipe.Instructions)
+	s, err := json.Marshal(recipe.Ingredients)
+	if err != nil {
+		return err
+	}
+	_, err = store.Db.Query("INSERT INTO recipes(name, description, ingredients, people, instructions) VALUES ($1,$2,$3,$4,$5)", recipe.Name, recipe.Description, s, recipe.People, recipe.Instructions)
 	return err
 }
--- internal/database/UpdateRecipeDB.go
+++ internal/database/UpdateRecipeDB.go
@@ -8,6 +8,9 @@ import (
 // UpdateRecipe : updates a recipe from the database
 func (store *DbStore) UpdateRecipe(recipe *model.Recipe, name string) error {
 	s, err := json.Marshal(recipe.Ingredients)
+	if err != nil {
+		return err
+	}
 	_, err = store.Db.Exec("UPDATE recipes SET name=$1, description=$2, ingredients=$3, people=$4, instructions=$5 WHERE name=$6", recipe.Name, recipe.Description, s, recipe.People, recipe.Instructions, name)
 	return err
 }
